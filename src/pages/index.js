13a5d7c52ce15c18700d0817cf8ed50bbe379df1
MarkMurphy37 <github@markmurphy.io>
2018-11-28 21:41:18 -0500

    Optimized images.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Layout from '../components/layout';
 import TypeWriter from '../components/typewriter';
 import { Link } from 'gatsby';
-
 import '../scss/pages/index.scss';
+import '../scss/components/button.scss'
 
 const IndexPage = () => (
   <Layout url={'/'}>
     <div className='index-wrapper'>
       <div className='index-header'>
-        <TypeWriter elementId={'terminal-line-1'} startDelay={1000} strings={['<h1>Mark Murphy</h1>']}/>
-        <TypeWriter elementId={'terminal-line-2'} startDelay={2000} strings={['<h2>Software Developer</h2>']}/>
+        <TypeWriter elementId={'typewriter-line-1'} startDelay={1000} strings={['<h1>Mark Murphy</h1>']}/>
+        <TypeWriter elementId={'typewriter-line-2'} startDelay={2000} strings={['<h2>Software Developer</h2>']}/>
       </div>
       <div className='index-links'>
           <Link to="/about" className='default-button'>
