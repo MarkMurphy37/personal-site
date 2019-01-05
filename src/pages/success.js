29d2d6cb82915ef3a7a84c8fa040aec4c8dba7d7
MarkMurphy37 <github@markmurphy.io>
2019-01-05 18:55:26 -0500

    Updated success page content and styling.

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Layout from '../components/layout';
 import Helmet from 'react-helmet';
 import Emoji from '../components/emoji';
+import '../scss/pages/success.scss';
 
 const Success = props => (
 	<Layout url="/success">
 		<Helmet>
 			<title>🙌🏼 Success - MarkMurphy.io</title>
 		</Helmet>
-		<h1>
-			Success! <Emoji symbol="🙌🏼" />
-		</h1>
-		<div>Thank you for reaching out. I will be in touch shortly!</div>
+		<div className="success-wrapper">
+			<h1>
+				Success! <Emoji symbol="🙌🏼" />
+			</h1>
+			<div className="success-content">Thank you for reaching out. I will be in touch shortly!</div>
+		</div>
 	</Layout>
 );
 
